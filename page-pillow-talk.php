7ea3d90f9a07e843bbdb8274ad4666c5b30573c1
nico581 <nico581v@stud.kea.dk>
2021-12-08 12:29:13 +0100

    Start

---
+++ page-pillow-talk.php
@@ -0,0 +1,189 @@
+<?php
+get_header();
+?>
+
+<style>
+
+    :root {
+        --burgundy: #6B2020; 
+        --rose: #E3C0C0;
+    }
+
+p {
+        font-family: 'Epilogue', sans-serif;
+        font-size: 18px;
+    }
+
+.headline {
+    color: var(--burgundy);
+}
+
+.sub-header {
+    color: var(--rose);
+}
+
+    #space {
+        padding-top: 150px;
+    }
+
+    #sec_1 {
+        height: 500px;
+        background-color: lightblue;
+    }
+
+    #sec_2 .container{
+        width: 980px;
+        margin-top: 10rem;
+    }
+
+    #sec_3 .container{
+        width: 980px;
+        margin-top: 10rem;
+    }
+
+    #sec_4 {
+        margin-top: 10rem;
+    margin-bottom: 10rem;
+    background-color: white;
+    height: 600px;
+    }
+
+    #sec_4 .container{
+        width: 980px;
+
+    }
+
+    #sec_5 .container{
+        width: 980px;
+    }
+
+    #sec_6 .container{
+        width: 980px;
+        margin-top: 10rem;
+    }
+
+    #sec_7 .container{
+        width: 980px;
+    }
+
+    #sec_7 {
+        margin-top: 10rem;
+        margin-bottom: 10rem;
+    }
+
+  
+</style>
+
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Epilogue:wght@300;400&display=swap" rel="stylesheet"> 
+
+  <body>
+      <div id="space"></div>
+      <section id="sec_1" class="d-flex justify-content-center align-items-center">
+      <div class="container-fluid">
+          <div class="row">
+              <div class="col">
+                  <h1 class="headline text-center fs-1">Sleep. You need it.</h1>
+                  <h2 class="sub-header text-center fs-5">W. Nicoline Christiansen</h2>
+                  <img class="logo" src="https://via.placeholder.com/50x50" alt="logo">
+              </div>
+          </div>
+      </div>
+      </section>
+      <section id="sec_2">
+          <div class="container">
+              <div class="row">
+                  <div class="col-12">
+                      <p class="fs-4 fw-bold">“I couldn’t wrap my head around why I felt so exhausted all the time. I was eating healthy, check. Exercised on a regular basis, check. I made sure to give me a break from my studies, check.”
+</p>
+                      <p>As an influencer and a student, 21-year-old Nicoline has a busy everyday life. Balancing her studies at Copenhagens school of Design and technology, while maintaining an influencer career on Instagram, requires plenty of strategic focus.
+
+</p>
+                  </div>
+              </div>
+          </div>
+      </section>
+      <section id="sec_3">
+          <div class="container">
+              <div class="row">
+                  <div class="col-7">
+                      <img src="https://via.placeholder.com/600x500" alt="Room">
+                  </div>
+                  <div class="col-5 d-flex flex-column justify-content-center">
+                      <h4>The shut down </h4>
+                      <p>“My health is so important to me. It was so frustrating not to know the answers. I nearly booked an appointment at the doctor, thinking I lacked vitamins or minerals. Or even something worse than that”</p>
+                      <p>Nicoline’s health took a drastic turn during her exam period. Unable to stay concentrated and feeling tired throughout the day were two of the main issues. Her 3’rd semester exam was coming up. Right when she needed her head and body to stay in harmony the most, everything shut down.</p>
+                  </div>
+              </div>
+          </div>
+      </section>
+      <section id="sec_4" class="d-flex flex-column justify-content-center align-items-center">
+          <div class="container">
+              <div class="row">
+                  <h4 class="text-center fs-1">“Sleep problems are important for health on an equal footing with eating healthy and exercising”</h4>
+                  <p class="text-center mt-3">- Torben Jager Petersen, sleep specialist</p>
+              </div>
+          </div>
+      </section>
+      <section id="sec_5">
+          <div class="container">
+              <div class="row">
+                  <div class="col-5">
+                      <h4>The awakening</h4>
+                      <p>“One day I was browsing through Spotify’s playlist to find one that would keep me focused. I stumbled upon Sleepin’ playlist “Off to Dreamland” and I finally understood what I was missing. My full 7-8 hour sleep. My head calmed down more than ever before, I was about to fall asleep at the library. I didn’t work a lot that day. I went home and put myself to bed at 20:00” </p>
+                      <p>The majority of health authorities around the world recommend eating fruits, greens, plenty of fibers, and exercising. But somehow, sleep tends to be forgotten on that list.</p>
+                      <p>“Sleep problems are important for health on an equal footing with eating healthy and exercising” says Torben Jager Petersen, sleep specialist.</p>
+                  </div>
+                  <div class="col-7">
+                      <img src="https://via.placeholder.com/600x800
+" alt="Room_2">
+                  </div>
+              </div>
+          </div>
+      </section>
+      <section id="sec_6">
+          <div class="container d-flex flex-row justify-content-center">
+              <div class="row">
+                  <div class="col">
+                      <img src="https://via.placeholder.com/600x800
+" alt="Room">
+                  </div>
+              </div>
+          </div>
+      </section>
+      <section id="sec_7">
+          <div class="container">
+              <div class="row">
+                  <div class="col">
+                      <p>Nicoline is now guiding her followers and friends to better sleep with our tips and tricks.
+</p>
+<p>“My view on sleep has truly changed. I’ve always known sleep was an important part, but I never knew how fundamental it was for my overall health. And with the Sleepin’ Green Oak bedding, I sleep softer than ever.”</p>
+                      <h4 class="fs-3 mt-5">Our high-quality cotton bedding only gets softer with every wash. </h4>
+                  </div>
+              </div>
+          </div>
+      </section>
+
+
+
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <script>
+</script>
+
+  </body>
+</html>
+
+
+
+
+
+<?php
+get_footer();
+?>
