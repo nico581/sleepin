b9756884aba3e947f54e80bc056dc8a2f6c31ebd
nico581 <nico581v@stud.kea.dk>
2021-12-16 14:18:52 +0100

    Rettelser

--- page-pillow-talk.php
+++ page-pillow-talk.php
@@ -7,14 +7,42 @@ get_header();
     :root {
         --burgundy: #6B2020; 
         --rose: #E3C0C0;
+        --navy: #062360;
+        --lightblue: #a0c1ff;
     }
+
+    a {
+  text-decoration: none!important
+  ;
+  color: var(--navy)!important;
+}
+
+a:hover {
+  color: var(--lightblue)!important;
+} 
+#body {
+  height: auto;
+  width: 100vw;
+  overflow-x: hidden;
+  padding: 0;
+  margin: 0;
+}
 
 p {
         font-family: 'Epilogue', sans-serif;
         font-size: 18px;
     }
 
+    .quote {
+        color: var(--navy);
+    }
+
 .headline {
+    color: var(--burgundy);
+    font-size: 60px;
+}
+
+.subtitle {
     color: var(--burgundy);
 }
 
@@ -28,16 +56,17 @@ p {
 
     #sec_1 {
         height: 500px;
-        background-color: lightblue;
+        background-image: url("https://nicolinechristiansen.dk/kea/3sem-eksamen-gruppe-12/wordpress/wp-content/themes/Divi-child/img/spalsh_pilow.png");
+    background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-position: center;
     }
 
     #sec_2 .container{
-        width: 980px;
         margin-top: 10rem;
     }
 
     #sec_3 .container{
-        width: 980px;
         margin-top: 10rem;
     }
 
@@ -46,6 +75,30 @@ p {
     margin-bottom: 10rem;
     background-color: white;
     height: 600px;
+    }
+
+
+
+
+    #sec_6 .container{
+
+        margin-top: 10rem;
+    }
+
+
+    #sec_7 {
+        margin-top: 10rem;
+        margin-bottom: 10rem;
+    }
+
+    @media screen and (min-width: 1080px) {
+
+        #sec_2 .container{
+        width: 980px;
+    }
+
+    #sec_3 .container{
+        width: 980px;
     }
 
     #sec_4 .container{
@@ -59,18 +112,13 @@ p {
 
     #sec_6 .container{
         width: 980px;
-        margin-top: 10rem;
     }
 
     #sec_7 .container{
         width: 980px;
     }
 
-    #sec_7 {
-        margin-top: 10rem;
-        margin-bottom: 10rem;
     }
-
   
 </style>
 
@@ -82,15 +130,15 @@ p {
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Epilogue:wght@300;400&display=swap" rel="stylesheet"> 
 
-  <body>
-      <div id="space"></div>
+<section id="body">
+<div id="space"></div>
       <section id="sec_1" class="d-flex justify-content-center align-items-center">
-      <div class="container-fluid">
+      <div class="splash container-fluid">
           <div class="row">
               <div class="col">
-                  <h1 class="headline text-center fs-1">Sleep. You need it.</h1>
-                  <h2 class="sub-header text-center fs-5">W. Nicoline Christiansen</h2>
-                  <img class="logo" src="https://via.placeholder.com/50x50" alt="logo">
+                  <h1 class="headline text-center">Sleep. You need it.</h1>
+                  <h2 class="sub-header text-center fs-5">Pillow talk with Nicoline Christiansen</h2>
+                  <div class="d-flex flex-row justify-content-center"> <img class="logo" src="https://nicolinechristiansen.dk/kea/3sem-eksamen-gruppe-12/wordpress/wp-content/themes/Divi-child/img/moonstars.png" alt="logo"></div>
               </div>
           </div>
       </div>
@@ -99,7 +147,7 @@ p {
           <div class="container">
               <div class="row">
                   <div class="col-12">
-                      <p class="fs-4 fw-bold">“I couldn’t wrap my head around why I felt so exhausted all the time. I was eating healthy, check. Exercised on a regular basis, check. I made sure to give me a break from my studies, check.”
+                      <p class="quote fs-4 fw-bold">“I couldn’t wrap my head around why I felt so exhausted all the time. I was eating healthy, check. Exercised on a regular basis, check. I made sure to give me a break from my studies, check.”
 </p>
                       <p>As an influencer and a student, 21-year-old Nicoline has a busy everyday life. Balancing her studies at Copenhagens school of Design and technology, while maintaining an influencer career on Instagram, requires plenty of strategic focus.
 
@@ -111,11 +159,11 @@ p {
       <section id="sec_3">
           <div class="container">
               <div class="row">
-                  <div class="col-7">
-                      <img src="https://via.placeholder.com/600x500" alt="Room">
+                  <div class="col-12 col-md-7">
+                      <img src="https://nicolinechristiansen.dk/kea/3sem-eksamen-gruppe-12/wordpress/wp-content/themes/Divi-child/img/bedroom.png" alt="Room">
                   </div>
-                  <div class="col-5 d-flex flex-column justify-content-center">
-                      <h4>The shut down </h4>
+                  <div class="col-12 col-md-5 d-flex flex-column justify-content-center">
+                      <h4 class="subtitle">The shut down </h4>
                       <p>“My health is so important to me. It was so frustrating not to know the answers. I nearly booked an appointment at the doctor, thinking I lacked vitamins or minerals. Or even something worse than that”</p>
                       <p>Nicoline’s health took a drastic turn during her exam period. Unable to stay concentrated and feeling tired throughout the day were two of the main issues. Her 3’rd semester exam was coming up. Right when she needed her head and body to stay in harmony the most, everything shut down.</p>
                   </div>
@@ -125,7 +173,7 @@ p {
       <section id="sec_4" class="d-flex flex-column justify-content-center align-items-center">
           <div class="container">
               <div class="row">
-                  <h4 class="text-center fs-1">“Sleep problems are important for health on an equal footing with eating healthy and exercising”</h4>
+                  <h4 class="quote text-center fs-1">“Sleep problems are important for health on an equal footing with eating healthy and exercising”</h4>
                   <p class="text-center mt-3">- Torben Jager Petersen, sleep specialist</p>
               </div>
           </div>
@@ -133,15 +181,14 @@ p {
       <section id="sec_5">
           <div class="container">
               <div class="row">
-                  <div class="col-5">
-                      <h4>The awakening</h4>
+                  <div class="col-12 col-md-5">
+                      <h4 class="subtitle">The awakening</h4>
                       <p>“One day I was browsing through Spotify’s playlist to find one that would keep me focused. I stumbled upon Sleepin’ playlist “Off to Dreamland” and I finally understood what I was missing. My full 7-8 hour sleep. My head calmed down more than ever before, I was about to fall asleep at the library. I didn’t work a lot that day. I went home and put myself to bed at 20:00” </p>
                       <p>The majority of health authorities around the world recommend eating fruits, greens, plenty of fibers, and exercising. But somehow, sleep tends to be forgotten on that list.</p>
                       <p>“Sleep problems are important for health on an equal footing with eating healthy and exercising” says Torben Jager Petersen, sleep specialist.</p>
                   </div>
-                  <div class="col-7">
-                      <img src="https://via.placeholder.com/600x800
-" alt="Room_2">
+                  <div class="col-12 col-md-7">
+                      <img src="https://nicolinechristiansen.dk/kea/3sem-eksamen-gruppe-12/wordpress/wp-content/themes/Divi-child/img/vindueskam.png">
                   </div>
               </div>
           </div>
@@ -149,8 +196,8 @@ p {
       <section id="sec_6">
           <div class="container d-flex flex-row justify-content-center">
               <div class="row">
-                  <div class="col">
-                      <img src="https://via.placeholder.com/600x800
+                  <div class="col-12">
+                      <img src="https://nicolinechristiansen.dk/kea/3sem-eksamen-gruppe-12/wordpress/wp-content/themes/Divi-child/img/collage.png
 " alt="Room">
                   </div>
               </div>
@@ -158,30 +205,26 @@ p {
       </section>
       <section id="sec_7">
           <div class="container">
-              <div class="row">
-                  <div class="col">
+              <div class="row d-flex flex-column-reverse flex-md-row">
+                  <div class="col-12">
                       <p>Nicoline is now guiding her followers and friends to better sleep with our tips and tricks.
 </p>
 <p>“My view on sleep has truly changed. I’ve always known sleep was an important part, but I never knew how fundamental it was for my overall health. And with the Sleepin’ Green Oak bedding, I sleep softer than ever.”</p>
-                      <h4 class="fs-3 mt-5">Our high-quality cotton bedding only gets softer with every wash. </h4>
+                      <h4 class="subtitle fs-3 mt-5">Our high-quality cotton bedding only gets softer with every wash. </h4>
                   </div>
               </div>
           </div>
       </section>
+</section>
+     
 
 
 
 
     <!-- Option 1: Bootstrap Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-    <script>
-</script>
-
-  </body>
-</html>
-
-
-
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.8.0/gsap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.8.0/ScrollTrigger.min.js"></script>
 
 
 <?php
